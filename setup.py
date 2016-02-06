7620b89ea40c4b68e4b1bc2abc896fe164e2986e
vineethv <vineethv@INBlr-vineeth.local>
2016-02-07 02:20:09 +0530

    [Deployment and RPM] Spikes
    - Adding a setup.py
    
    Would want to see how to generate the RPM
    using bdist rpm, and then use fabric to deploy
    the RPM to a remote machine.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Flask-Examples',
+      version='1.0',
+      description='Flask playground for testing stuff',
+      author='Vineeth Venudasan',
+      author_email='vineethv@tw.com',
+      url='https://www.python.org/',
+      packages=['flaskexamples'],
+     )
